9d7d4f3731925412c5dad12345b37d00a457a6ea
amichels <andy.j.michels@gmail.com>
2015-02-12 21:33:30 -0600

    added audio

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
+	var audio = document.getElementById('js-controls');
 	$(".js-play-bt").click(function(){
+		var album = $(this).attr("data-album");
 		$(".player").addClass("active");
+		$(".label").css("background-image","url(images/"+album+".jpg)");
+		audio.setAttribute('src', 'audio/'+album+'.mp3');
+		audio.play();
+		audio.onplay = function(){
+			$(".player").addClass("play");
+		};
+		audio.onpause = function(){
+			$(".player").removeClass("play");
+		};
+	});
+	$("#js-player").click(function(){
+		$(".player").removeClass("active");
+		audio.pause();
 	});
 });
